7b7822f4819220468e076db49c6cb34b05e644af
Joel2B <jbuenouas@gmail.com>
2022-05-22 18:56:05 -0600

    Update examples

--- examples/example.php
+++ examples/example.php
@@ -3,5 +3,5 @@
 $data = file_get_contents('https://appsdev.cyou/xv-ph-rt/api/?site_id=xvideos&video_id=59934029');
 
 echo '<pre>';
-print_r(json_decode($data, true));
+echo json_encode(json_decode($data), JSON_PRETTY_PRINT);
 echo '</pre>';
